b5697ace10e60da428df37f268750eb9d1ed4b8a
Thinspeed <offmailno5@gmail.com>
2022-04-14 00:16:11 +0300

    Add task editing page

--- 1.js
+++ 1.js
@@ -15,8 +15,21 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', process.env.VIEWS);
 app.use(express.static(path.join(__dirname, process.env.STATIC)));
-app.get('/', async (req, res) => {
-    const rows = (await db.query('SELECT * FROM task')).rows;
+app.get('/', bodyParser.urlencoded(), async (req, res) => {
+    if (req.query['id']) {
+        const row = 
+        (await db.query('SELECT * FROM task WHERE id=$1', [ req.query['id'] ])).rows[0];
+        res.render('task', { task : new Task(
+            row['id'],
+            row['name'],
+            row['status'],
+            new Date(row['date']),
+        )});
+
+        return;
+    }
+
+    const rows = (await db.query('SELECT * FROM task ORDER BY id')).rows;
     const tasks = [];
     for (let i = 0; i < rows.length; i++) {
         tasks.push(new Task(
@@ -27,18 +40,23 @@ app.get('/', async (req, res) => {
         ));
     }
 
-    console.log(tasks);
-    res.render('main', { a : tasks });
+    res.render('main', { tasks : tasks });
 })
 
 app.post('/', bodyParser.urlencoded(), async (req, res) => {
-    console.log(req.body);
-    id += 1;
-    let date = req.body.date.split('-');
-    let t = await db.query('INSERT INTO task (name, date, status) values ($1, $2, $3)', 
-        [ req.body.name, new Date(date[0], date[1], date[2]), Status.Created]);
+    let status = req.body.status;
+    console.log(status);
+    if (status == Status.Done || status == Status.Canceled) {
+        await db.query('UPDATE task SET status=$1 WHERE id=$2', [ status, req.query.id ]);
+    }
+    else if (status == Status.Created && req.body.name != '' && req.body.date == '') {
+        id += 1;
+        console.log(req.body);
+        let date = req.body.date.split('-');
+        await db.query('INSERT INTO task (name, date, status) values ($1, $2, $3)', 
+            [ req.body.name, new Date(date[0], date[1], date[2]), status ]);
+    }
 
-    console.log(t);
     res.redirect('/');
 });
 
